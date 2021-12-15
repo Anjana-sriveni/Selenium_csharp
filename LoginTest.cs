2c04ba2d18e2ffdd5d3a9d532418dc22b13f0ab6
Anjana <76049893+Anjana-sriveni@users.noreply.github.com>
2021-12-15 12:41:05 +0530

    TestCases

---
+++ LoginTest.cs
@@ -0,0 +1,46 @@
+﻿using Docker.DotNet.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using IndigoTesting.PageObjectModel;
+
+
+namespace IndigoTesting.TestCases
+{
+    [TestFixture]
+    class LoginTest : BasePage
+    {
+        [Test]
+        public void Test()
+        {
+
+            var homepage = new HomePage(driver);
+            homepage.NavigateToLoginPage();
+            Thread.Sleep(4000);
+
+            var loginpage = new LoginPage(driver);
+            loginpage.LoginDetails("1234567890", "hahahah");
+            Thread.Sleep(4000);
+
+            var backtohomepg = new BackToHomePage(driver);
+            backtohomepg.NavigateTohomepage();
+            Thread.Sleep(4000);
+
+            //driver.Navigate().Refresh();
+            //driver.Navigate().GoToUrl("https://www.goindigo.in/");
+            driver.Navigate().Back();
+            
+            var bookflightpage = new BookFlightPage(driver);
+            bookflightpage.FillTheDetails();
+            Thread.Sleep(4000);
+
+
+        }
+    }
+}
